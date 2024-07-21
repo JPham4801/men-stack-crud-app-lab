f6c2e2ad9a5764d2b6b07dc5b07da0f079fc92e3
JPham4801 <JPham4801@gmail.com>
2024-07-21 05:53:15 -0500

    init commit

--- models/food.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const foodSchema = new mongoose.Schema({
-  name: String,
-  type: String,
-  isFavorite: Boolean
-})
-
-module.exports = mongoose.model('Food', foodSchema)
